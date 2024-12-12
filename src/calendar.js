4cde1ce73f9c6c712d2abb5f5a4f30c465eab2db
daniyal-0 <150108495+daniyal-0@users.noreply.github.com>
2024-12-11 18:32:47 -0800

    Update calendar.js

--- src/calendar.js
+++ src/calendar.js
@@ -8,17 +8,52 @@ var months = ["January", "February", "March", "April", "May", "June",
 var monthDisplay = document.querySelector(".current-month-year");
 var previousButton = document.querySelector(".calendar-controls .prev");
 var nextButton = document.querySelector(".calendar-controls .next");
-var dayCells = document.querySelectorAll(".calendar-table td");
+var calendarBody = document.querySelector(".calendar-table tbody");
 
-//Display the current month & year
-monthDisplay.textContent = months[monthIndex] + " " + year;
+function generateCalendar() {
+  calendarBody.innerHTML = "";
+  var firstDay = new Date(year, monthIndex, 1).getDay();
 
-//Highlight current day (if present on calendar)
-var dateNumbers = document.querySelectorAll(".date-number");
-for (var i = 0; i < dateNumbers.length; i++) {
-    var dayNumber = Number(dateNumbers[i].textContent);
-    if (dayNumber === now.getDate()) {
-        dateNumbers[i].parentElement.classList.add("today");
+  //num days in a month
+  var daysInMonth = new Date(year, monthIndex + 1, 0).getDate();
+
+  //rows for each week
+  var row = document.createElement("tr");
+  var dayCount = 0;
+  for (var i = 0; i < firstDay; i++) {
+    var cell = document.createElement("td");
+    row.appendChild(cell);
+    dayCount++;
+  }
+
+  for (var i = 1; i <= daysInMonth; i++) {
+    var cell = document.createElement("td");
+    cell.innerHTML = i;
+
+    //today's date (check if true)
+    if (i === now.getDate() && monthIndex === now.getMonth() && year === now.getFullYear()) {
+      cell.classList.add("today");
     }
+
+    row.appendChild(cell);
+    dayCount++;
+
+    if (dayCount === 7) {
+      calendarBody.appendChild(row);
+      row = document.createElement("tr");
+      dayCount = 0;
+    }
+  }
+
+  if (dayCount > 0) {
+    for (var i = dayCount; i < 7; i++) {
+      var cell = document.createElement("td");
+      row.appendChild(cell);
+    }
+    calendarBody.appendChild(row);
+  }
+
+  monthDisplay.textContent = months[monthIndex] + " " + year;
 }
 
+generateCalendar();
